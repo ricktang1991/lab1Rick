a927b2aac640203091491ef344abeda706b43daa
ricktang1991 <55897267+ricktang1991@users.noreply.github.com>
2019-10-16 03:13:50 -0700

    fix

--- Lab1Rick/src/lab/lab1/Driver.java
+++ Lab1Rick/src/lab/lab1/Driver.java
@@ -3,6 +3,7 @@ package lab.lab1;
 public class Driver {
     public static void main(String[] args) {
         Model1 a = new Model1("Susan", "Smith", 70, 120, false, true);
+        a.printDetails();
         a.displayModelDetails();
     }
 }
